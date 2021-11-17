25052b3e7fb0da18ce85abff5e23027e50db1e52
JuanS072 <juancarlossequeira07@gmail.com>
2021-11-17 18:50:05 -0300

    Depo

--- PI-Videogames-main/api/src/db.js
+++ PI-Videogames-main/api/src/db.js
@@ -33,6 +33,7 @@ dialectOptions:{
 
 
 
+
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
